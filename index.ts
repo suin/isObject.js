399a398974654fb2ace54ae5f06ffa303e68c898
suin <suinyeze@gmail.com>
2020-05-19 11:30:58 +0900

    docs: ✏️ add API documentation

--- index.ts
+++ index.ts
@@ -1,3 +1,11 @@
+/**
+ * Return if the given `value` is a type `object`.
+ *
+ * This function gives the TypeScript compiler the typing-hint that the given
+ * `value` is possibly the type of the given `T`.
+ *
+ * @param value
+ */
 export const isObject = <T extends object>(
   value: unknown,
 ): value is { [P in keyof T]?: unknown } =>
